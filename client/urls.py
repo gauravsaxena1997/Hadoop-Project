500430da9cdffde0b66ff1970515558c6f7e1ede
gaurav <gauravsaxena.cs@gmail.com>
2018-06-30 02:20:59 +0530

    hadoop client for docker based cluster

--- client/urls.py
+++ client/urls.py
@@ -13,4 +13,16 @@ urlpatterns = [
     url(r'^existing/$', views.existing, name='existing'),
     url(r'^del_client/$', views.del_client, name='del_client'),
 
+    # Operations
+    url(r'^ls/$', views.ls, name='ls'),
+    url(r'^mkdir/$', views.mkdir, name='mkdir'),
+    url(r'^rmr/$', views.rmr, name='rmr'),
+    url(r'^touchz/$', views.touchz, name='touchz'),
+    url(r'^rm/$', views.rm, name='rm'),
+    url(r'^put/$', views.put, name='put'),
+    url(r'^cat/$', views.cat, name='cat'),
+    url(r'^chown/$', views.chown, name='chown'),
+    url(r'^count/$', views.count, name='count'),
+    url(r'^clear_all/$', views.clear_all, name='clear_all'),
+
 ]
--- client/views.py
+++ client/views.py
@@ -43,11 +43,13 @@ def home (request):
 	client_name = request.session.get('client_name') 
 	client_ip = request.session.get('client_ip') 
 	client_id = request.session.get('client_id') 
+	command = request.session.get('command') 
+	request.session['command'] = None
 	return render (request,'client/home.html',{
 		'client_name':client_name,
 		'client_ip':client_ip,
-		'client_id':client_id
-		})
+		'client_id':client_id,
+		'command':command           })
 
 def del_client (request):
 	os.system('docker kill client')
@@ -58,18 +60,71 @@ def del_client (request):
 	request.session['client'] = None
 	return redirect ('/client/')
 
+def ls (request):
+	client_name = request.session.get('client_name')
+	command = sb.getoutput('docker exec '+client_name+' hadoop fs -ls hdfs://172.17.0.2:10001/')
+	request.session['command'] = command
+	return redirect ('/client/')
 
+def cat (request):
+	location = request.POST.get('location')
+	client_name = request.session.get('client_name')
+	command = sb.getoutput('docker exec '+client_name+' hadoop fs -cat hdfs://172.17.0.2:10001'+location)
+	request.session['command'] = command
+	return redirect ('/client/')
 
+def mkdir (request):
+	location = request.POST.get('location')
+	client_name = request.session.get('client_name')
+	sb.getoutput('docker exec '+client_name+' hadoop fs -mkdir hdfs://172.17.0.2:10001'+location)
+	return redirect ('/client/')
 
+def rmr (request):
+	location = request.POST.get('location')
+	client_name = request.session.get('client_name')
+	sb.getoutput('docker exec '+client_name+' hadoop fs -rmr hdfs://172.17.0.2:10001'+location)
+	return redirect ('/client/')
 
+def touchz (request):
+	location = request.POST.get('location')
+	client_name = request.session.get('client_name')
+	sb.getoutput('docker exec '+client_name+' hadoop fs -touchz hdfs://172.17.0.2:10001'+location)
+	return redirect ('/client/')
 
+def rm (request):
+	location = request.POST.get('location')
+	client_name = request.session.get('client_name')
+	sb.getoutput('docker exec '+client_name+' hadoop fs -rm hdfs://172.17.0.2:10001'+location)
+	return redirect ('/client/')
 
+def clear_all (request):
+	all_files = sb.getoutput ('hadoop fs -ls hdfs://172.17.0.2:10001/ | cut -d "/" -f2 | grep -vi Found')
+	directories = all_files.split()
+	for files in directories:
+		print ('cleaning /'+files+' ...')
+		sb.getoutput ('hadoop fs -rmr hdfs://172.17.0.2:10001/'+files)
+	return redirect ('/client/')
 
+def put (request):
+	location_from = request.POST.get('location_from')
+	location_to = request.POST.get('location_to')
+	client_name = request.session.get('client_name')
+	sb.getoutput('docker exec '+client_name+' hadoop fs -put '+location_from+ ' hdfs://172.17.0.2:10001'+location_to)
+	return redirect ('/client/')
 
+def chown (request):
+	user = request.POST.get('user')
+	location = request.POST.get('location')
+	client_name = request.session.get('client_name')
+	sb.getoutput('docker exec '+client_name+' hadoop fs -chown '+user+ ' hdfs://172.17.0.2:10001'+location)
+	return redirect ('/client/')
 
-
-
-
+def count (request):
+	location = request.POST.get('location')
+	client_name = request.session.get('client_name')
+	command = sb.getoutput('docker exec '+client_name+' hadoop fs -count hdfs://172.17.0.2:10001'+location)
+	request.session['command'] = command
+	return redirect ('/client/')
 
 
 
